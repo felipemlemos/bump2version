df0fe85bffa092dc60e029b22fe60fc3b0ceba02
Felipe Lemos <felipelemos@gmail.com>
2022-12-22 22:54:20 +0100

    Add hadolint

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="bump2version",
-    version="1.0.2-dev",
-    url="https://github.com/c4urself/bump2version",
-    author="Christian Verkerk",
+    version="1.0.3",
+    url="https://github.com/felipemlemos/bump2version",
+    author="Felipe Lemos",
     author_email="christianverkerk@ymail.com",
     license="MIT",
     packages=["bumpversion"],
@@ -27,7 +27,7 @@ setup(
             "bump2version = bumpversion.cli:main",
         ]
     },
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -35,10 +35,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],

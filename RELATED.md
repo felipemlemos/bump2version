2c2d7ccaffea0feb3750a7c42d797f78da1a686e
Felipe Lemos <felipelemos@gmail.com>
2022-12-22 22:07:44 +0100

    Add pre-commit

--- RELATED.md
+++ RELATED.md
@@ -3,18 +3,18 @@
 * [bumpversion](https://pypi.org/project/bumpversion/) is the original project
   off of which `bump2version` was forked.  We'll be merging
   back with them at some point (issue [#86](https://github.com/c4urself/bump2version/issues/86)).
-  
+
 * [tbump](https://github.com/tankerhq/tbump) is a complete rewrite, with a nicer UX and additional features, like running commands (aka hooks) before or after the bump. It only works for Git repos right now.
 
 * [ADVbumpversion](https://github.com/andrivet/advbumpversion) is another fork.
-  It offered some features that are now incorporated by its author into `bump2version`. 
+  It offered some features that are now incorporated by its author into `bump2version`.
   This fork is thus now deprecated, and it recommends to use `bump2version`
   (issue [#121](https://github.com/c4urself/bump2version/issues/121)).
 
 * [zest.releaser](https://pypi.org/project/zest.releaser/) manages
   your Python package releases and keeps the version number in one location.
 
-* [setuptools-scm](https://pypi.org/project/setuptools-scm/) relies on 
+* [setuptools-scm](https://pypi.org/project/setuptools-scm/) relies on
   version control tags and the state of your working copy to determine
   the version number.
 
@@ -27,19 +27,19 @@
   semantic versioning.
 
 * [python-semantic.release](https://github.com/relekang/python-semantic-release)
-  automatically bumps your (semantic) version number based on the 
+  automatically bumps your (semantic) version number based on the
   types of commits (breaking/new/bugfix) in your source control.
-  
+
 * [PyCalVer](https://gitlab.com/mbarkhau/pycalver) is very similar to bump2version, but with support for [calendar based versioning](https://calver.org/).
 
 
 ## Change log building
-  
+
 * [towncrier](https://pypi.org/project/towncrier/) assembles a changelog
   file from multiple snippets found in individual (merge) commits.
-  
+
 * [releases](https://pypi.org/project/releases/) helps build a Sphinx
   ReStructuredText changelog.
-  
+
 * [gitchangelog](https://pypi.org/project/gitchangelog/) searches
   the git commit history to make a configurable changelog file.
--- bumpversion/cli.py
+++ bumpversion/cli.py
@@ -1,5 +1,4 @@
 import argparse
-from datetime import datetime
 import glob
 import io
 import itertools
@@ -9,38 +8,16 @@ import re
 import sre_constants
 import sys
 import warnings
-from configparser import (
-    ConfigParser,
-    RawConfigParser,
-    NoOptionError,
-)
+from configparser import ConfigParser, NoOptionError, RawConfigParser
+from datetime import datetime
 
-from bumpversion import __version__, __title__
-from bumpversion.version_part import (
-    VersionConfig,
-    NumericVersionPartConfiguration,
-    ConfiguredVersionPartConfiguration,
-)
-from bumpversion.exceptions import (
-    IncompleteVersionRepresentationException,
-    MissingValueForSerializationException,
-    WorkingDirectoryIsDirtyException,
-)
-
-from bumpversion.utils import (
-    ConfiguredFile,
-    DiscardDefaultIfSpecifiedAppendAction,
-    keyvaluestring,
-    prefixed_environ,
-)
+from bumpversion import __title__, __version__
+from bumpversion.exceptions import IncompleteVersionRepresentationException, MissingValueForSerializationException, WorkingDirectoryIsDirtyException
+from bumpversion.utils import ConfiguredFile, DiscardDefaultIfSpecifiedAppendAction, keyvaluestring, prefixed_environ
 from bumpversion.vcs import Git, Mercurial
+from bumpversion.version_part import ConfiguredVersionPartConfiguration, NumericVersionPartConfiguration, VersionConfig
 
-
-DESCRIPTION = "{}: v{} (using Python v{})".format(
-    __title__,
-    __version__,
-    sys.version.split("\n")[0].split(" ")[0]
-)
+DESCRIPTION = "{}: v{} (using Python v{})".format(__title__, __version__, sys.version.split("\n")[0].split(" ")[0])
 VCS = [Git, Mercurial]
 
 # detect either
@@ -49,9 +26,7 @@ VCS = [Git, Mercurial]
 # bumpversion:file(suffix):value
 # bumpversion:file ( suffix with spaces):value
 RE_DETECT_SECTION_TYPE = re.compile(
-    r"^bumpversion:"
-    r"((?P<file>file|glob)(\s*\(\s*(?P<file_suffix>[^\):]+)\)?)?|(?P<part>part)):"
-    r"(?P<value>.+)",
+    r"^bumpversion:" r"((?P<file>file|glob)(\s*\(\s*(?P<file_suffix>[^\):]+)\)?)?|(?P<part>part)):" r"(?P<value>.+)",
 )
 
 logger_list = logging.getLogger("bumpversion.list")
@@ -87,11 +62,11 @@ def main(original_args=None):
         explicit_config = known_args.config_file
     config_file = _determine_config_file(explicit_config)
     config, config_file_exists, config_newlines, part_configs, files = _load_configuration(
-        config_file, explicit_config, defaults,
+        config_file,
+        explicit_config,
+        defaults,
     )
-    known_args, parser2, remaining_argv = _parse_arguments_phase_2(
-        args, known_args, defaults, root_parser
-    )
+    known_args, parser2, remaining_argv = _parse_arguments_phase_2(args, known_args, defaults, root_parser)
     version_config = _setup_versionconfig(known_args, part_configs)
     current_version = version_config.parse(known_args.current_version)
     context = dict(
@@ -104,9 +79,7 @@ def main(original_args=None):
     )
 
     # calculate the desired new version
-    new_version = _assemble_new_version(
-        context, current_version, defaults, known_args.current_version, positionals, version_config
-    )
+    new_version = _assemble_new_version(context, current_version, defaults, known_args.current_version, positionals, version_config)
     args, file_names = _parse_arguments_phase_3(remaining_argv, positionals, defaults, parser2)
     new_version = _parse_new_version(args, new_version, version_config)
 
@@ -116,24 +89,24 @@ def main(original_args=None):
 
     # replace version in target files
     vcs = _determine_vcs_dirty(VCS, defaults)
-    files.extend(
-        ConfiguredFile(file_name, version_config)
-        for file_name
-        in (file_names or positionals[1:])
-    )
+    files.extend(ConfiguredFile(file_name, version_config) for file_name in (file_names or positionals[1:]))
     _check_files_contain_version(files, current_version, context)
     _replace_version_in_files(files, current_version, new_version, args.dry_run, context)
     _log_list(config, args.new_version)
 
     # store the new version
     _update_config_file(
-        config, config_file, config_newlines, config_file_exists, args.new_version, args.dry_run,
+        config,
+        config_file,
+        config_newlines,
+        config_file_exists,
+        args.new_version,
+        args.dry_run,
     )
 
     # commit and tag
     if vcs:
-        context = _commit_to_vcs(files, context, config_file, config_file_exists, vcs,
-                                 args, current_version, new_version)
+        context = _commit_to_vcs(files, context, config_file, config_file_exists, vcs, args, current_version, new_version)
         _tag_in_vcs(vcs, context, args)
 
 
@@ -149,9 +122,7 @@ def split_args_in_optional_and_positional(args):
         if i > 0:
             previous = args[i - 1]
 
-        if (not arg.startswith("-")) and (
-            previous not in OPTIONAL_ARGUMENTS_THAT_TAKE_VALUES
-        ):
+        if (not arg.startswith("-")) and (previous not in OPTIONAL_ARGUMENTS_THAT_TAKE_VALUES):
             positions.append(i)
 
     positionals = [arg for i, arg in enumerate(args) if i in positions]
@@ -161,13 +132,10 @@ def split_args_in_optional_and_positional(args):
 
 
 def _parse_arguments_phase_1(original_args):
-    positionals, args = split_args_in_optional_and_positional(
-        sys.argv[1:] if original_args is None else original_args
-    )
+    positionals, args = split_args_in_optional_and_positional(sys.argv[1:] if original_args is None else original_args)
     if len(positionals[1:]) > 2:
         warnings.warn(
-            "Giving multiple files on the command line will be deprecated, "
-            "please use [bumpversion:file:...] in a config file.",
+            "Giving multiple files on the command line will be deprecated, " "please use [bumpversion:file:...] in a config file.",
             PendingDeprecationWarning,
         )
     root_parser = argparse.ArgumentParser(add_help=False)
@@ -219,7 +187,7 @@ def _setup_logging(show_list, verbose):
         log_level = [logging.WARNING, logging.INFO, logging.DEBUG][verbose]
     except IndexError:
         log_level = logging.DEBUG
-    root_logger = logging.getLogger('')
+    root_logger = logging.getLogger("")
     root_logger.setLevel(log_level)
     logger.debug("Starting %s", DESCRIPTION)
 
@@ -288,17 +256,13 @@ def _load_configuration(config_file, explicit_config, defaults):
     for listvaluename in ("serialize",):
         try:
             value = config.get("bumpversion", listvaluename)
-            defaults[listvaluename] = list(
-                filter(None, (x.strip() for x in value.splitlines()))
-            )
+            defaults[listvaluename] = list(filter(None, (x.strip() for x in value.splitlines())))
         except NoOptionError:
             pass  # no default value then ;)
 
     for boolvaluename in ("commit", "tag", "dry_run"):
         try:
-            defaults[boolvaluename] = config.getboolean(
-                "bumpversion", boolvaluename
-            )
+            defaults[boolvaluename] = config.getboolean("bumpversion", boolvaluename)
         except NoOptionError:
             pass  # no default value then ;)
 
@@ -327,12 +291,10 @@ def _load_configuration(config_file, explicit_config, defaults):
                 )
                 ThisVersionPartConfiguration = ConfiguredVersionPartConfiguration
 
-            if config.has_option(section_name, 'independent'):
-                section_config['independent'] = config.getboolean(section_name, 'independent')
+            if config.has_option(section_name, "independent"):
+                section_config["independent"] = config.getboolean(section_name, "independent")
 
-            part_configs[section_value] = ThisVersionPartConfiguration(
-                **section_config
-            )
+            part_configs[section_value] = ThisVersionPartConfiguration(**section_config)
         elif section_type.get("file"):
             filename = section_value
 
@@ -340,29 +302,20 @@ def _load_configuration(config_file, explicit_config, defaults):
                 section_config["serialize"] = list(
                     filter(
                         None,
-                        (
-                            x.strip().replace("\\n", "\n")
-                            for x in section_config["serialize"].splitlines()
-                        ),
+                        (x.strip().replace("\\n", "\n") for x in section_config["serialize"].splitlines()),
                     )
                 )
 
             section_config["part_configs"] = part_configs
 
             if "parse" not in section_config:
-                section_config["parse"] = defaults.get(
-                    "parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
-                )
+                section_config["parse"] = defaults.get("parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)")
 
             if "serialize" not in section_config:
-                section_config["serialize"] = defaults.get(
-                    "serialize", ["{major}.{minor}.{patch}"]
-                )
+                section_config["serialize"] = defaults.get("serialize", ["{major}.{minor}.{patch}"])
 
             if "search" not in section_config:
-                section_config["search"] = defaults.get(
-                    "search", "{current_version}"
-                )
+                section_config["search"] = defaults.get("search", "{current_version}")
 
             if "replace" not in section_config:
                 section_config["replace"] = defaults.get("replace", "{new_version}")
@@ -377,9 +330,7 @@ def _load_configuration(config_file, explicit_config, defaults):
 
 
 def _parse_arguments_phase_2(args, known_args, defaults, root_parser):
-    parser2 = argparse.ArgumentParser(
-        prog="bumpversion", add_help=False, parents=[root_parser]
-    )
+    parser2 = argparse.ArgumentParser(prog="bumpversion", add_help=False, parents=[root_parser])
     parser2.set_defaults(**defaults)
     parser2.add_argument(
         "--current-version",
@@ -391,9 +342,7 @@ def _parse_arguments_phase_2(args, known_args, defaults, root_parser):
         "--parse",
         metavar="REGEX",
         help="Regex parsing the version string",
-        default=defaults.get(
-            "parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
-        ),
+        default=defaults.get("parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"),
     )
     parser2.add_argument(
         "--serialize",
@@ -438,9 +387,7 @@ def _setup_versionconfig(known_args, part_configs):
     return version_config
 
 
-def _assemble_new_version(
-    context, current_version, defaults, arg_current_version, positionals, version_config
-):
+def _assemble_new_version(context, current_version, defaults, arg_current_version, positionals, version_config):
     new_version = None
     if "new_version" not in defaults and arg_current_version:
         try:
@@ -549,18 +496,14 @@ def _parse_arguments_phase_3(remaining_argv, positionals, defaults, parser2):
         metavar="TAG_MESSAGE",
         dest="tag_message",
         help="Tag message",
-        default=defaults.get(
-            "tag_message", "Bump version: {current_version} → {new_version}"
-        ),
+        default=defaults.get("tag_message", "Bump version: {current_version} → {new_version}"),
     )
     parser3.add_argument(
         "--message",
         "-m",
         metavar="COMMIT_MSG",
         help="Commit message",
-        default=defaults.get(
-            "message", "Bump version: {current_version} → {new_version}"
-        ),
+        default=defaults.get("message", "Bump version: {current_version} → {new_version}"),
     )
     parser3.add_argument(
         "--commit-args",
@@ -573,9 +516,7 @@ def _parse_arguments_phase_3(remaining_argv, positionals, defaults, parser2):
         assert defaults["files"] is not None
         file_names = defaults["files"].split(" ")
     parser3.add_argument("part", help="Part of the version to be bumped.")
-    parser3.add_argument(
-        "files", metavar="file", nargs="*", help="Files to change", default=file_names
-    )
+    parser3.add_argument("files", metavar="file", nargs="*", help="Files to change", default=file_names)
     args = parser3.parse_args(remaining_argv + positionals)
 
     if args.dry_run:
@@ -635,7 +576,12 @@ def _log_list(config, new_version):
 
 
 def _update_config_file(
-        config, config_file, config_newlines, config_file_exists, new_version, dry_run,
+    config,
+    config_file,
+    config_newlines,
+    config_file_exists,
+    new_version,
+    dry_run,
 ):
     config.set("bumpversion", "current_version", new_version)
     new_config = io.StringIO()
@@ -656,20 +602,14 @@ def _update_config_file(
                 f.write(new_config.getvalue().strip() + "\n")
 
     except UnicodeEncodeError:
-        warnings.warn(
-            "Unable to write UTF-8 to config file, because of an old configparser version. "
-            "Update with `pip install --upgrade configparser`."
-        )
+        warnings.warn("Unable to write UTF-8 to config file, because of an old configparser version. " "Update with `pip install --upgrade configparser`.")
 
 
-def _commit_to_vcs(files, context, config_file, config_file_exists, vcs, args,
-                   current_version, new_version):
+def _commit_to_vcs(files, context, config_file, config_file_exists, vcs, args, current_version, new_version):
     commit_files = [f.path for f in files]
     if config_file_exists:
         commit_files.append(config_file)
-    assert vcs.is_usable(), "Did find '{}' unusable, unable to commit.".format(
-        vcs.__name__
-    )
+    assert vcs.is_usable(), "Did find '{}' unusable, unable to commit.".format(vcs.__name__)
     do_commit = args.commit and not args.dry_run
     logger.info(
         "%s %s commit",
@@ -693,8 +633,8 @@ def _commit_to_vcs(files, context, config_file, config_file_exists, vcs, args,
     }
     context.update(time_context)
     context.update(prefixed_environ())
-    context.update({'current_' + part: current_version[part].value for part in current_version})
-    context.update({'new_' + part: new_version[part].value for part in new_version})
+    context.update({"current_" + part: current_version[part].value for part in current_version})
+    context.update({"new_" + part: new_version[part].value for part in new_version})
     context.update(special_char_context)
 
     commit_message = args.message.format(**context)
